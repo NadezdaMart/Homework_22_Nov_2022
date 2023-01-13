49afa8b476ac5d4ac1b33bb7cdf3f27dd7c53f99
nadezdamartynkevic <nadezda.martynkevic@gmail.com>
2023-01-13 15:05:46 +0000

    homework

---
+++ script.js
@@ -0,0 +1,64 @@
+
+// Task1
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// let sum = 0
+// arr.forEach(function(i) {
+//     sum = i + sum
+// })
+// console.log(sum)
+
+
+// Task2
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// let rez = arr.every(function(i) {
+//     if (i > 0) {
+//       return i  
+//     }
+    
+// })
+// console.log(rez)
+
+//Task3
+
+// let arr = ["один", "два", "три", "четыре", "пять", "шесть"]
+// let newArr = arr.map(str => str.length>5)
+// console.log(newArr)
+
+
+
+// Task4
+
+// let arr = [1, -2, 0, -4, -7]
+// let newArr = arr.filter(function(i) 
+//     {return i<0
+
+// })
+// console.log(newArr.length)
+
+
+
+
+
+// Task5 
+
+//     let arr = [2, 4, 6, 8, 10, 12]
+//     let numberOfElements
+//     let newArr = arr.reduce(function(sum, i) {
+//         return sum > 10 
+            
+//     }
+//     )   
+//     Дальше не понимаю, что нужно вернуть. Объясните пожалуйста в коментариях.   
+
+
+
+// Task7
+
+// let arr = [2, -2, 0, -4, -7]
+// let newArr = arr.filter(function(i)
+// {return i >0})
+// console.log(newArr)
+// let rez = Math.sqrt(newArr)
+// console.log(rez)
